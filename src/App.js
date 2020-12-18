bd0ec04840e7764ef4182d8f3a77ffe15e0ab40a
ducktheduck <66016734+ducktheduck@users.noreply.github.com>
2020-12-18 13:14:24 +0200

    Creating a button that counts.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React, {Component} from 'react'
 
 class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+
+    }
+    componentWillMount(props, state){
+
+    }
+    componentDidMount(props, state){
+        console.log('Mounted with: ', props, state)
+    }
+    componentWillReceiveProps(props){
+        console.log(props)
+    }
+
+    componentWillUpdate(props, state){
+        if(this.props.name !== props.name){
+            //ce fa ma aicia
+        }
+
+    }
+
+    componentDidUpdate(props, state){
+        if(this.state.count > 10 && this.state.count != state.count && !this.state.overTen){
+            console.log('updating over ten')
+            this.setState({overTen: true});
+        }
+    }
+
+    handleClick = () => {
+        this.setState({count: this.state.count + 1})
+    }
 
     render(){
-        let name = "jonu"
+        let {count} = this.state
         return(
             <div>
-                <h1>buna zaua {name}</h1>
-                <span> ce fa wa</span>
+                <h1>Ai apasat pe buton de {count} ori</h1>
+                {(this.state.overTen) ?
+                    <h3>Beat high score of 10!</h3>
+                 : null
+                 }
+                
+                <span>
+                    <button onClick = {() => this.handleClick()}>
+                        apasa ma
+                    </button>
+                </span>
             </div>
         );
     }
